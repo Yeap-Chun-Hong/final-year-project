be7640b1908be5c86965e5cf174dd241ae1c32ec
Yeap-Chun-Hong <chunhong@veecotech.com>
2022-06-03 02:28:40 +0800

    Removing unnecessary part in index

--- index.php
+++ index.php
@@ -80,7 +80,7 @@ $result2 = mysqli_query($dbc,$query2);
 								echo '<p><span> Email: </span>'.$email.'</p>';
 								echo '<p><span> Phone Number: </span>'.$phone.'</p>';
 								echo '</section>';
-								echo '<a href="#" class="view-hotel-btn">View hotel</a>';
+								echo '<a href="single_hotel.php?id='.$id.'" class="view-hotel-btn">View hotel</a>';
 								echo '</div>';
 								echo '</div>';
 
@@ -100,140 +100,7 @@ $result2 = mysqli_query($dbc,$query2);
 		
 
 		
-		<section class="page-heading">
-			<div class="container">
-				<h2>upcomming events</h2>
-			</div>
-		</section>
-		<section class="events-section" itemprop="event" itemscope itemtype=" http://schema.org/Event">
-			<div class="container">
-				<div class="event-wrap">
-					<div class="img-wrap" itemprop="image">
-						<img src="images/events.jpg" alt="event images">
-					</div>
-					<div class="details">
-						<a href=""><h3 itemprop="name">Orientation Programme for new Students.</h3></a>
-						<p itemprop="description">Orientation Programme for new sffs Students. Orientation Programme for new sffs Students. Orientation Programme for new sffs Students.</p>
-
-						<h5 itemprop="startDate"><i class="far fa-clock"></i> Dec 30,2018 | 11am</h5>
-						<h5 itemprop="location"><i class="fas fa-map-marker-alt"></i> Hotel Malla, Lainchaur</h5>
-					</div>
-				</div>
-
-				<div class="event-wrap">
-					<div class="img-wrap" itemprop="image">
-						<img src="images/events.jpg" alt="event images">
-					</div>
-					<div class="details">
-						<a href=""><h3 itemprop="name">Orientation Programme for new Students.</h3></a>
-						<p itemprop="description">Orientation Programme for new sffs Students. Orientation Programme for new sffs Students. Orientation Programme for new sffs Students.</p>
-
-						<h5 itemprop="startDate"><i class="far fa-clock"></i> Dec 30,2018 | 11am</h5>
-						<h5 itemprop="location"><i class="fas fa-map-marker-alt"></i> Hotel Malla, Lainchaur</h5>
-					</div>
-				</div>
-			</div>
-		</section>
-		<!-- End of Events section -->
-		<section class="what-other-say">
-			<div class="container">
-				<h4 class="article-subtitle">Get to Know</h4>
-				<h2 class="head">what our customer say</h2>
-				<div class="three owl-carousel owl-theme">
-					<div class="customer-item" itemscope itemtype="http://schema.org/Rating">
-						<div class="border">
-							<div class="customer">
-								<figure>
-									<img class="customer-img" src="images/kimberly1.jpg" alt="Person image">
-									<figcaption>
-										<span itemprop="author">SAGAR KUMAR SAPKOTA</span>
-										<div class="rateYo" itemprop="ratingValue"></div>
-									</figcaption>
-								</figure>
-							</div>
-							<div class="customer-review">
-								<p itemprop="description">
-									"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-									consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-									cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non"
-								</p>
-							</div>
-						</div>
-					</div>
-					<div class="customer-item" itemscope itemtype="http://schema.org/Rating">
-						<div class="border">
-							<div class="customer">
-								<figure>
-									<img class="customer-img" src="images/customer-img.jpg" alt="Person image">
-									<figcaption>
-										<span itemprop="author">SAGAR KUMAR SAPKOTA</span>
-										<div class="rateYo" itemprop="ratingValue"></div>
-									</figcaption>
-								</figure>
-							</div>
-							<div class="customer-review">
-								<p itemprop="description">
-									"22222Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-									consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-									cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non"
-								</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-		<!-- End of Others talk -->
-		<section class="page-heading">
-			<div class="container">
-				<h2>latest news</h2>
-			</div>
-		</section>
-		<section class="latest-news">
-			<div class="container" itemprop="event" itemscope itemtype=" http://schema.org/Event">
-				<div class="owl-two owl-carousel">
-					<div class="news-wrap" itemprop="event">
-						<div class="news-img-wrap" itemprop="image">
-							<img src="images/latest-new-img.jpg" alt="Latest News Images">
-						</div>
-						<div class="news-detail" itemprop="description">
-							<a href=""><h1>Orientation Programme for new Students.</h1></a>
-							<h2 itemprop="startDate">By Admin | 20 Dec. 2018</h2>
-
-							<p>Orientation Programme for new sffs Students. Orientatin Programmes for new Students.. Orientatin Programmes for new Students</p>
-						</div>
-					</div>
-
-					<div class="news-wrap" itemprop="event">
-						<div class="news-img-wrap" itemprop="image">
-							<img src="images/latest-new-img.jpg" alt="Latest News Images">
-						</div>
-						<div class="news-detail" itemprop="description">
-							<a href=""><h1>Orientation Programme for new Students.</h1></a>
-							<h2 itemprop="startDate">By Admin | 20 Dec. 2018</h2>
-
-							<p>Orientation Programme for new sffs Students. Orientatin Programmes for new Students.. Orientatin Programmes for new Students</p>
-						</div>
-					</div>
-
-					<div class="news-wrap" itemprop="event">
-						<div class="news-img-wrap" itemprop="image">
-							<img src="images/latest-new-img.jpg" alt="Latest News Images">
-						</div>
-						<div class="news-detail" itemprop="description">
-							<a href=""><h1>Orientation Programme for new Students.</h1></a>
-							<h2 itemprop="startDate">By Admin | 20 Dec. 2018</h2>
-
-							<p>Orientation Programme for new sffs Students. Orientatin Programmes for new Students.. Orientatin Programmes for new Students</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
+		
 		<!-- Latest News CLosed -->
 		<?php include("footer.php");?>
 		
